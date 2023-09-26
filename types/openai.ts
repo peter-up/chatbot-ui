7146d084443fc99d7499c012ab619fa1aabc9499
Peter <7362306+peter-up@users.noreply.github.com>
2023-09-26 15:03:36 +0800

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -10,6 +10,7 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo-0613',
   GPT_3_5_16K = 'gpt-3.5-turbo-16k-0613',
+  GPT_3_5_instruct = 'gpt-3.5-turbo-instruct',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
@@ -30,6 +31,12 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-3.5-16K',
     maxLength: 48000,
     tokenLimit: 16000,
+  },
+  [OpenAIModelID.GPT_3_5_instruct]: {
+    id: OpenAIModelID.GPT_3_5_instruct,
+    name: 'GPT-3.5-struct',
+    maxLength: 12000,
+    tokenLimit: 4000,
   },
   [OpenAIModelID.GPT_3_5_AZ]: {
     id: OpenAIModelID.GPT_3_5_AZ,
